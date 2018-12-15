6b672bbd43857bce401dce99c258773d6db4a7f1
Arbo Lindma <arbo.lindma@gmail.com>
2018-12-15 20:39:32 +0200

    added autoconfigure module

--- src/main/java/schultz/dustin/io/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/JustGifItApplication.java
@@ -1,68 +1,17 @@
 package schultz.dustin.io;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.filter.HttpPutFormContentFilter;
-import org.springframework.web.filter.RequestContextFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
 
 @SpringBootApplication(exclude = {JacksonAutoConfiguration.class,
         JmxAutoConfiguration.class})
 public class JustGifItApplication {
-
-    @Value("${multipart.location}/gif/")
-    private String gifLocation;
-
     public static void main(String[] args) {
         SpringApplication.run(JustGifItApplication.class, args);
     }
 
-    @PostConstruct
-    private void init() {
-        File gifFolder = new File(gifLocation);
-        if (!gifFolder.exists()) {
-            gifFolder.mkdir();
-        }
-    }
 
-    @Bean
-    public FilterRegistrationBean
-    deRegisterHiddenHttpMethodfilter(HiddenHttpMethodFilter filter){
-        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
-        bean.setEnabled(false);
-        return bean;
-    }
-
-    @Bean
-    public FilterRegistrationBean
-        deRegisterRequestContextFilter(RequestContextFilter filter){
-        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
-        bean.setEnabled(false);
-        return bean;
-    }
-
-
-
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/gif/**")
-                        .addResourceLocations("file:" + gifLocation);
-                super.addResourceHandlers(registry);
-            }
-        };
-    }
 }
--- src/main/java/schultz/dustin/io/controller/UploadController.java
+++ src/main/java/schultz/dustin/io/controller/UploadController.java
@@ -1,5 +1,8 @@
 package schultz.dustin.io.controller;
 
+import com.justgifit.services.ConverterService;
+import com.justgifit.services.GifEncoderService;
+import com.justgifit.services.VideoDecoderService;
 import com.madgag.gif.fmsware.AnimatedGifEncoder;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FrameGrabber;
@@ -9,9 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import schultz.dustin.io.services.ConverterService;
-import schultz.dustin.io.services.GifEncoderService;
-import schultz.dustin.io.services.VideoDecoderService;
 
 import javax.inject.Inject;
 import java.io.File;
